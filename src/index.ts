2e7102f0b2b67a2756bd670073aa591da8ca5d2f
Gwion Tangi <lil.smiss7@gmail.com>
2022-11-02 16:51:46 +0100

    new route and authentication

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {PORT} from './config/constants';
 import {router} from './routes/recipe';
 import { routerAuthenticate } from './routes/authenticate';
 import {routerUser} from './routes/user';
+import { routerIngredient } from './routes/ingredient';
 
 const app = express();
 const allowOrigins = ['http://localhost:8100'];
@@ -15,22 +16,33 @@ const options: cors.CorsOptions = {
     origin: allowOrigins
 };
 
+if(process.env.NODE_ENV !== 'production'){
+    console.log('Le token JWT :', generateToken("tanguy", "gwion.tanguy@my-digital-school.org", "Administrateur"));
+}
+
 app.use(cors())
 app.use(express.json());
-console.log('Le token JWT :', generateToken());
 
 app.get('/recipe/show/:id', router);
 app.get('/recipe/showone/:name', router);
+app.get('/recipe/list/:id', router);
 
 app.post('/recipe/add', router);
 
 app.put('/recipe/update/:id', router);
 
+app.get('/ingredient/show/:id', routerIngredient);
+app.get('/ingredient/showone/:name', routerIngredient);
+
+app.post('/ingredient/add', routerIngredient);
+
+app.put('/ingredient/update/:id', routerIngredient);
+
 app.get('/user/show/:id', routerUser);
 app.get('/user/showone/:lastname', routerUser);
 
-app.post('/signin', routerAuthenticate);
-app.post('/login', routerAuthenticate);
+app.post('/user/signin', routerAuthenticate);
+app.post('/user/login', routerAuthenticate);
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
